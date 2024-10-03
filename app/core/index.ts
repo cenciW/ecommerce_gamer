212cf7188ad6945b796aa2f40ee4043201e72dd0
cenciW <ze.ccenci@gmail.com>
2024-10-03 15:17:25 +0100

    core: parte 1 ok

--- app/core/index.ts
+++ app/core/index.ts
@@ -1,3 +1,7 @@
 //pagina para exportar arquivos e arquitetura ficar melhor
 //deve ser independente de framework e lib
 //núcleo da aplicação
+
+export * from "./produto";
+export * from "./parcelamento";
+export * from "./constants";
---
+++ app/core/parcelamento/CalcularParcelamento.ts
@@ -0,0 +1,37 @@
+import { QTDE_MAX_PARCELAS, TAXA_JUROS_MENSAL } from "../constants";
+import Parcelamento from "./Parcelamento";
+
+export default class CalcularParcelamento {
+  executar(
+    valor: number,
+    qtdeParcelas: number = QTDE_MAX_PARCELAS,
+    taxaJuros: number = TAXA_JUROS_MENSAL
+  ): Parcelamento {
+    if (qtdeParcelas < 2 || qtdeParcelas > QTDE_MAX_PARCELAS) {
+      throw new Error(`Quantidade de parcelas inválida: ${qtdeParcelas}`);
+    }
+    const totalComJuros = this.calcularJurosCompostos(
+      valor,
+      taxaJuros,
+      qtdeParcelas
+    );
+
+    return {
+      valorParcela: this.comDuasCasasDecimais(totalComJuros / qtdeParcelas),
+      valorTotal: this.comDuasCasasDecimais(totalComJuros),
+      qtdeParcelas,
+      taxaJuros,
+    };
+  }
+
+  private calcularJurosCompostos(
+    valorTotal: number,
+    taxaJuros: number,
+    qtdeParcelas: number
+  ): number {
+    return valorTotal * Math.pow(1 + taxaJuros, qtdeParcelas);
+  }
+  private comDuasCasasDecimais(valor: number): number {
+    return Math.round(valor * 100) / 100;
+  }
+}
