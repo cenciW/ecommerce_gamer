6505b142712614d67a1610afa5aabe1320a6982c
cenciW <ze.ccenci@gmail.com>
2024-10-06 11:30:46 +0100

    doing Rodape Component

---
+++ app/components/template/Cabecalho.tsx
@@ -0,0 +1,15 @@
+import Logo from "../shared/Logo";
+import IconeCarrinho from "../shared/IconeCarrinho";
+import Link from "next/link";
+// import useCarrinho from "@/data/hooks/useCarrinho";
+
+export default function Cabecalho() {
+  //   const { qtdeItens } = useCarrinho();
+  const qtdeItens = 0;
+  return (
+    <div
+      className="flex flex-col h-20"
+      style={{ background: "linear-gradient(90deg, #" }}
+    ></div>
+  );
+}
