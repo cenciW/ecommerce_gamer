5b26d21b49898dfea56bc343766c0416615d0791
cenciW <ze.ccenci@gmail.com>
2024-10-06 12:00:27 +0100

    adding group of routes and putting produtos inside

--- app/(paginas)/layout.tsx
+++ app/(paginas)/layout.tsx
@@ -1,5 +1,6 @@
 import Pagina from "../components/template/Pagina";
 
 export default function Layout(props: any) {
+  //sem o pagina, o layout não terá o cabeçalho e rodapé
   return <Pagina>{props.children}</Pagina>;
 }
---
+++ app/(paginas)/page.tsx
@@ -0,0 +1,13 @@
+import ProdutoItem from "@/app/components/produto/ProdutoItem";
+import { produtos } from "@/app/core";
+import Pagina from "../components/template/Pagina";
+
+export default function Home() {
+  return (
+    <div className="container grid grid-cols-4">
+      {produtos.map((produto) => (
+        <ProdutoItem key={produto.id} produto={produto} />
+      ))}
+    </div>
+  );
+}
