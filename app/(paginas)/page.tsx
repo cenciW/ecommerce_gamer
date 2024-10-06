3926026c5dfcda68641db41d87cffdb238a399d7
cenciW <ze.ccenci@gmail.com>
2024-10-06 12:14:52 +0100

    adding class button to globals.css and adding ListaProdutos and ProdutoNaoEncontrado logics

--- app/(paginas)/page.tsx
+++ app/(paginas)/page.tsx
@@ -1,13 +1,12 @@
 import ProdutoItem from "@/app/components/produto/ProdutoItem";
 import { produtos } from "@/app/core";
 import Pagina from "../components/template/Pagina";
+import ListaProdutos from "../components/produto/ListaProdutos";
 
 export default function Home() {
   return (
-    <div className="container grid grid-cols-4">
-      {produtos.map((produto) => (
-        <ProdutoItem key={produto.id} produto={produto} />
-      ))}
+    <div className="flex-1 flex flex-col container gap-5 py-10">
+      <ListaProdutos />
     </div>
   );
 }
--- app/(paginas)/produto/[id]/page.tsx
+++ app/(paginas)/produto/[id]/page.tsx
@@ -1,11 +1,14 @@
+import ProdutoNaoEncontrado from "@/app/components/produto/ProdutoNaoEncontrado";
 import { produtos } from "@/app/core";
 
 export default function PaginaProduto(props: any) {
   const id = +props.params.id;
   const produto = produtos.find((produto) => produto.id === id);
-  return (
+  return produto ? (
     <div>
       <h1>Produto: {produto?.nome}</h1>
     </div>
+  ) : (
+    <ProdutoNaoEncontrado />
   );
 }
